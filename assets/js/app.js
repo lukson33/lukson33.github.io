7007eccd08eaf4840c308c34249dc65d55cb3d23
lukson33 <45272632+lukson33@users.noreply.github.com>
2019-02-15 16:45:10 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,80 @@
+// const navSlide = () => {
+//   const burger = document.querySelector(".burger");
+//   const nav = document.querySelector(".nav-links");
+//   const navLinks = document.querySelectorAll(".nav-links li");
+
+//   //Toggle nav
+//   burger.addEventListener("click", () => {
+//     //Toggle nav
+//     nav.classList.toggle("nav-active");
+
+//     //Animate links
+//     navLinks.forEach((link, index) => {
+//       if (link.style.animation) {
+//         link.style.animation = "";
+//       } else {
+//         const delay = (link.style.animation = `navLinkFade 0.5s ease forwards ${index /
+//           11 +
+//           0.5}s`);
+//         console.log(delay);
+//       }
+//     });
+//     //Burger Animation
+//     burger.classList.toggle("toggle");
+//   });
+// };
+
+// if (nav.classList[1] === "nav-active") {
+//   console.log("active");
+// } else {
+//   console.log("not-active");
+// }
+
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
+  console.log(nav.classList);
+  let isOpen = false;
 
   //Toggle nav
   burger.addEventListener("click", () => {
     //Toggle nav
     nav.classList.toggle("nav-active");
+    if (isOpen) {
+      isOpen = false;
+    } else {
+      isOpen = true;
+    }
 
-    //Animate links
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
+    if (isOpen) {
+      //Animate links
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          const delay = (link.style.animation = `navLinkFade 0.5s ease forwards ${index /
+            11 +
+            0.5}s`);
+          console.log(delay);
+        }
+      });
+      console.log("opened");
+    } else if (!isOpen) {
+      navLinks.forEach(link => {
         link.style.animation = "";
-      } else {
-        const delay = (link.style.animation = `navLinkFade 0.5s ease forwards ${index /
-          11 +
-          0.5}s`);
-        console.log(delay);
-      }
+      });
+    }
+
+    navLinks.forEach(link => {
+      link.addEventListener("click", function() {
+        nav.classList.remove("nav-active");
+        isOpen = false;
+        burger.classList.remove("toggle");
+        navLinks.forEach(link => {
+          link.style.animation = "";
+        });
+      });
     });
     //Burger Animation
     burger.classList.toggle("toggle");
@@ -37,6 +94,15 @@ document.addEventListener("scroll", () => {
 });
 
 navSlide();
+
+const navLinks = document.querySelectorAll(".nav-links li");
+const nav = document.querySelector(".nav-links");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", function() {
+    nav.classList.remove("nav-active");
+  });
+});
 
 //JQUERY SLIDE
 $(".click-info").click(function() {
