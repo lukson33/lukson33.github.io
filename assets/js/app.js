673dfff374bcac4bdd6511bc1120b302c99f9fb5
lukson33 <45272632+lukson33@users.noreply.github.com>
2019-02-16 00:23:19 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,6 +30,36 @@
 //   console.log("not-active");
 // }
 
+/* REMOVE HOVER ON PHONES */
+if (screen.width < 800) {
+  function hasTouch() {
+    return (
+      "ontouchstart" in document.documentElement ||
+      navigator.maxTouchPoints > 0 ||
+      navigator.msMaxTouchPoints > 0
+    );
+  }
+
+  if (hasTouch()) {
+    // remove all :hover stylesheets
+    try {
+      // prevent exception on browsers not supporting DOM styleSheets properly
+      for (var si in document.styleSheets) {
+        var styleSheet = document.styleSheets[si];
+        if (!styleSheet.rules) continue;
+
+        for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+          if (!styleSheet.rules[ri].selectorText) continue;
+
+          if (styleSheet.rules[ri].selectorText.match(":hover")) {
+            styleSheet.deleteRule(ri);
+          }
+        }
+      }
+    } catch (ex) {}
+  }
+}
+
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
