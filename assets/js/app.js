8d62646c560b4ee411c5c515fd3e278d2f1f1ed4
lukson33 <45272632+lukson33@users.noreply.github.com>
2019-03-06 00:32:07 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,40 +28,6 @@ if (screen.width < 800) {
   }
 }
 
-var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
-
-function preventDefault(e) {
-  e = e || window.event;
-  if (e.preventDefault) e.preventDefault();
-  e.returnValue = false;
-}
-
-function preventDefaultForScrollKeys(e) {
-  if (keys[e.keyCode]) {
-    preventDefault(e);
-    return false;
-  }
-}
-
-function disableScroll() {
-  if (window.addEventListener)
-    // older FF
-    window.addEventListener("DOMMouseScroll", preventDefault, false);
-  window.onwheel = preventDefault; // modern standard
-  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-  window.ontouchmove = preventDefault; // mobile
-  document.onkeydown = preventDefaultForScrollKeys;
-}
-
-function enableScroll() {
-  if (window.removeEventListener)
-    window.removeEventListener("DOMMouseScroll", preventDefault, false);
-  window.onmousewheel = document.onmousewheel = null;
-  window.onwheel = null;
-  window.ontouchmove = null;
-  document.onkeydown = null;
-}
-
 /* NAVIGATION ANIMATION */
 const navSlide = () => {
   const burger = document.querySelector(".burger");
@@ -90,14 +56,10 @@ const navSlide = () => {
             0.4}s`);
         }
       });
-      //Prevent scrolling
-      disableScroll();
     } else if (!isOpen) {
       navLinks.forEach(link => {
         link.style.animation = "";
       });
-      //Enable scrolling again
-      enableScroll();
     }
 
     navLinks.forEach(link => {
