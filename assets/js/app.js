59411f4be4f8117f80b4de327ebce044f2562e1a
lukson33 <45272632+lukson33@users.noreply.github.com>
2019-03-06 00:20:17 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,6 +28,40 @@ if (screen.width < 800) {
   }
 }
 
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault) e.preventDefault();
+  e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+function disableScroll() {
+  if (window.addEventListener)
+    // older FF
+    window.addEventListener("DOMMouseScroll", preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove = preventDefault; // mobile
+  document.onkeydown = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+  if (window.removeEventListener)
+    window.removeEventListener("DOMMouseScroll", preventDefault, false);
+  window.onmousewheel = document.onmousewheel = null;
+  window.onwheel = null;
+  window.ontouchmove = null;
+  document.onkeydown = null;
+}
+
 /* NAVIGATION ANIMATION */
 const navSlide = () => {
   const burger = document.querySelector(".burger");
@@ -57,17 +91,13 @@ const navSlide = () => {
         }
       });
       //Prevent scrolling
-      $("body").css({ overflow: "hidden" });
-      $(document).bind("scroll", function() {
-        window.scrollTo(0, 0);
-      });
+      disableScroll();
     } else if (!isOpen) {
       navLinks.forEach(link => {
         link.style.animation = "";
       });
       //Enable scrolling again
-      $(document).unbind("scroll");
-      $("body").css({ overflow: "visible" });
+      enableScroll();
     }
 
     navLinks.forEach(link => {
