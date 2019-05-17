c47642ddcd8f5b5e01a0a583e6537f6a766c624d
lukson33 <45272632+lukson33@users.noreply.github.com>
2019-05-17 23:26:36 +0200

    Update translate-about.js

--- assets/js/translate-about.js
+++ assets/js/translate-about.js
@@ -5,7 +5,11 @@ const dataReload = document.querySelectorAll("[data-reload]");
 //Language translations
 const language = {
   eng: {
-    about: "ABOUT US",
+    menu: "MENU",
+    gallery: "GALLERY",
+    contact: "CONTACT",
+    main: "MAIN PAGE",
+    aboutText: "ABOUT US",
     who: "WHO IS CORNELIUS?",
     abs1: `An unusal scene took our breath away that morning while cleaning the attic of our great grandfather's home in Vračar. On the dusty floor, tucked away in the corner, lay an old overturned cylinder with a letter sticking out of its torn lining. Confusion and curiosity painted our faces as we read the contents of the yellow scrap paper. It looked as if it container centuries worth of memories. `,
     abs2: `"My name is Cornelius. I was born in the year 3985. For as long as I can remember, I have worn the uniform of Her Majesty The Queen of Adventure. I've travelled through forgotten times and encountered adventures of worlds yet to come. In the idleness of my unending existence, I've been a spaceship pilot, hunter, tailor, sailboat builder, cosmonaut, the royal family postmaster and so much more. A soldier by profession and an artist at heart, I took pleasure in creating culinary masterpieces. The shining smiles on friends' faces and the pure unadulterated bliss of our joint ventures were my greatest victories and the most precious spoils of war. Now, I'm the Defence Commander against boredom at the Ministry Against Stereotypes. If you happened upon my hat and this letter in it, the time has come for you to follow me. I invite you to, together, with the unbreakable power of imagination, build a new reality. We will invent a future in which wealth is no longer defined by what you have, but by your enjoyments. I await you here."`,
@@ -19,7 +23,13 @@ if (window.location.href.indexOf("about") > -1) {
   //Define language via window hash
   if (window.location.hash) {
     if (window.location.hash === "#eng") {
-      document.getElementById("abs-about").textContent = language.eng.about;
+      document.getElementById("lang-main").textContent = language.eng.main;
+      document.getElementById("lang-menu").textContent = language.eng.menu;
+      document.getElementById("lang-gallery").textContent =
+        language.eng.gallery;
+      document.getElementById("lang-contact").textContent =
+        language.eng.contact;
+      document.getElementById("abs-about").textContent = language.eng.aboutText;
       document.getElementById("abs-who").textContent = language.eng.who;
       document.getElementById("abs-eng").textContent = language.eng.abs1;
       document.getElementById("abs-eng-2").textContent = language.eng.abs2;
